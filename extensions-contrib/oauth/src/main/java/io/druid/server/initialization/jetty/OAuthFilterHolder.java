58b6dd0bc7e058ba7b8f01025c2b97e5dd640395
Keuntae Park <sirpkt@apache.org>
2016-11-09 18:23:59 +0900

    add OAuth2Filter

---
+++ extensions-contrib/oauth/src/main/java/io/druid/server/initialization/jetty/OAuthFilterHolder.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.druid.server.initialization.jetty;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+import com.metamx.http.client.HttpClient;
+import io.druid.guice.annotations.Global;
+import io.druid.server.security.OAuth.OAuthFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import java.util.EnumSet;
+import java.util.Map;
+
+public class OAuthFilterHolder implements ServletFilterHolder
+{
+  public static final String OVERLORD_QUERY_REQUEST_PATH = "/druid/indexer/v1/*";
+  private final HttpClient httpClient;
+
+  @Inject
+  public OAuthFilterHolder(
+      @Global HttpClient httpClient
+  )
+  {
+    this.httpClient = httpClient;
+  }
+
+  @Override
+  public Filter getFilter()
+  {
+    return new OAuthFilter(httpClient);
+  }
+
+  @Override
+  public Class<? extends Filter> getFilterClass()
+  {
+    return null;
+  }
+
+  @Override
+  public Map<String, String> getInitParameters()
+  {
+    return ImmutableMap.of();
+  }
+
+  @Override
+  public String getPath()
+  {
+    return OVERLORD_QUERY_REQUEST_PATH;
+  }
+
+  @Override
+  public EnumSet<DispatcherType> getDispatcherType()
+  {
+    return null;
+  }
+
+
+}
